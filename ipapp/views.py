b3b466705ba3e8a1d4c7b40676609e86f803468d
unknown <871214098@qq.com>
2019-06-19 18:23:16 +0800

    '更改请求url'

--- ipapp/views.py
+++ ipapp/views.py
@@ -2,36 +2,24 @@ from django.shortcuts import render, HttpResponse
 from ipapp.lib.qqwry import qqwry
 
 
-# Create your views here.
-
 def index(request):
-    #print(request.META)
-    #print(request)
-    # print(request.__dict__)
-    #ip = request.META.get('REMOTE_ADDR')
+    # ip = request.META.get('REMOTE_ADDR')
     ip = request.META.get('HTTP_X_FORWARDED_FOR')
     czip = qqwry.CzIp()
     str = czip.get_addr_by_ip(ip).split()
 
     if 'curl' in request.META.get('HTTP_USER_AGENT'):
-
-
         msg = """IP	: {}
 地址	: {}
 运营商	: {}
 """.format(ip, str[0], str[1])
     else:
-#         msg = """
-# <pre>IP	: {}
-# 地址	: {}
-# 运营商	: {}
-# </pre>""".format(ip,str[0], str[1])
         dic = {
-            'ip' : ip,
+            'ip': ip,
             'add': str[0],
             'isp': str[1],
         }
-        return  render(request,'index.html',{'dic':dic})
+        return render(request, 'index.html', {'dic': dic})
 
     return HttpResponse(msg)
 
@@ -45,19 +33,13 @@ def getip(request):
 def seach(request, ip):
     czip = qqwry.CzIp()
     str = czip.get_addr_by_ip(ip).split()
-
     if 'curl' in request.META.get('HTTP_USER_AGENT'):
-
         msg = """IP	: {}
     地址	: {}
     运营商	: {}
     """.format(ip, str[0], str[1])
     else:
-        #         msg = """
-        # <pre>IP	: {}
-        # 地址	: {}
-        # 运营商	: {}
-        # </pre>""".format(ip,str[0], str[1])
+
         dic = {
             'ip': ip,
             'add': str[0],
